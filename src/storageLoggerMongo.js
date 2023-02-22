c67fc30f7bf36f839cfd3186519551c4a5ff332d
Vandenynas <stlo.subs@gmail.com>
2023-02-22 12:45:14 +0100

    Implemented timeseries model

--- src/storageLoggerMongo.js
+++ src/storageLoggerMongo.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 const Pair = require('./models/pair')
+const Price = require('./models/price')
 
 mongoose.set('strictQuery', false)
 
@@ -27,46 +29,112 @@ const getObservationsArray = (observationsObject) => {
     return observationsArray
 }
 
-const updateObservationsData = (pair, rawObservationsData) => {
+const getExistingObservations = async (symbol, seconds) => {
+    console.log(`Getting existing observations for ${symbol}-${seconds}`)
+    const priceTimestamps =  await Price.find({
+        "metadata.symbol" : symbol,
+        "metadata.seconds": seconds,
+        timestamp: { $gt: new Date("2020-02-02")}
+    }, {timestamp: 1, _id: 0})
+
+    const existingTimestamps = priceTimestamps.map( timestampObject => {
+        return timestampObject.timestamp
+    })
+
+    return existingTimestamps
+}
+
+const updateTimeseries = async (pair, rawObservationsData, seconds) => {
+    console.log('Updating time series')
+    const beforeGettingTimestamps = new Date()
+    // const existingTimestamps = await getExistingObservations(pair.symbol, seconds)
+    // const afterGettingTimestamps = new Date()
+    // console.log(`Getting timestamps took ${afterGettingTimestamps - beforeGettingTimestamps}`)
+    // console.log(existingTimestamps)
+    const priceCollectionArray = []
+
+    // const duplicatedTimestamps = []
+
+    for (let timestamp in rawObservationsData) {
+
+        // if ( existingTimestamps.includes(new Date(parseInt(timestamp) * 1000))) {
+        //     duplicatedTimestamps.push(timestamp)
+        //     continue
+        // }
+        const observation = {
+            timestamp: new Date(parseInt(timestamp) * 1000),
+            price: parseFloat(rawObservationsData[timestamp].toPrecision(8)),
+            metadata: {
+                symbol: pair.symbol,
+                seconds: pair.priceData.timeframe.seconds
+            }
+        }
+        priceCollectionArray.push(observation)
+    }
+
+    // console.log(duplicatedTimestamps.length, Object.keys(rawObservationsData).length)
+
+    Price.collection.insertMany(priceCollectionArray).then(() => {
+        const afterInserting = new Date()
+        console.log(`${priceCollectionArray.length} series updated for ${pair.symbol}`)
+        console.log(`The update took ${(afterInserting - beforeGettingTimestamps) / 1000} seconds`)
+    })
+}
+
+const updateObservationsData = async (pair, rawObservationsData) => {
     const observationsArray = getObservationsArray(rawObservationsData)
 
     observationsArray.sort((a, b) => {
         return parseInt(b._id) - parseInt(a._id)
     })
 
-    const data = pair.priceData.observations
-
-    const maxUpdates = 10000
+    const maxUpdates = 100
+    let total = 1
     let counter = 0
 
-    for (let i = 0; i < observationsArray.length; i += 1) {
-        const observation = observationsArray[i]
-        if (counter > maxUpdates) {
-            break
-        }
-        
-        const dataPointExists = (dataPoint) => {
-            return dataPoint._id === observation._id
-        }
+    const existingTimestamps = pair.priceData.observations.map(dataPoint => {
+        return dataPoint._id
+    })
     
-        if( data.findIndex(dataPointExists) === -1) {
+    for(let newObservation of observationsArray) {
+        if(!existingTimestamps.includes(newObservation._id)) {
+            pair.priceData.observations.push(newObservation)
             console.clear()
-            console.log(`Updating observation ${i} of ${observationsArray.length} for ${pair.symbol}`)
-            data.push(observation)
+            console.log(`Adding observation ${total} of ${observationsArray.length}`)
             counter += 1
+
+            if (counter >= maxUpdates) {
+                counter = 0
+
+                const sortedArray = pair.priceData.observations.sort((a, b) => {
+                    return new Date(parseInt(a._id) * 1000) - new Date(parseInt(b._id) * 1000) 
+                })
+
+                pair.priceData.observations = [...sortedArray]
+
+                console.log(`Saving ${total} of ${observationsArray.length} in ${pair.symbol}`)
+
+                try {
+                    await pair.save()
+                    console.log(`Updated/saved ${pair.symbol}`)                   
+                } catch (error) {
+                    console.log(`Error updating pair ${pair.symbol}: ${error}`)
+                }
+            }
+            total += 1
         }
     }
 }
 
-async function appendObservations(pairSymbol, data) {
-    const timeframe = data.observationTimeframe
-
-    console.log('Appending', data.symbol)
+async function appendObservations(data, seconds) {
+    console.log('Appending...', data.symbol)
 
     Pair.findById(data.symbol)
         .then(pair => {
+            console.log(pair.symbol)
             if (pair !== null && pair !== undefined) {
-                updateObservationsData(pair, data.observations)
+                // updateObservationsData(pair, data.observations)
+                updateTimeseries(pair, data.observations, seconds)
 
             } else {
                     const {
@@ -88,27 +156,14 @@ async function appendObservations(pairSymbol, data) {
                         poolFee,
                         arrayTypes,
                         extraMinutesData,
-                        priceData: {
-                            _id: `${symbol}-${timeframe.seconds}`,
-                            timeframe,
-                            observations: [],
-                        }
                     })
 
-                updateObservationsData(pair, data.observations)
-
-                console.log(pair.priceData.earliestDate)
-
-                pair.save()
-                    .then(result => {
-                        console.log(`Created new pair for ${data.symbol}`)                   
-        
-                    })
-                    .catch(error => {
-                        console.log(`Error creating new pair ${data.symbol}: ${error}`)
-                    })
+                updateTimeseries(pair, data.observations, seconds)
             }
 
+        })
+        .catch(error => {
+            console.log(error)
         })
 }
 
